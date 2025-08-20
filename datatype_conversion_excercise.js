ec71e1717b24fd82f8552da5126dbe16d9acbe4e
Syed Saquib Quadri <Syed1saquib2quadri@gmail.com>
2025-08-20 15:23:16 +0000

    practise

---
+++ datatype_conversion_excercise.js
@@ -0,0 +1,48 @@
+/* 1. What will these print?
+
+console.log("5" - 3);
+console.log("5" + 3);
+console.log(true + 2);
+console.log(false == 0);
+console.log(null == 0);
+console.log(null == undefined);
+
+// 2
+// 53
+// 3
+// true/ 1
+// true ---> wrong --> false --->
+// true
+*/
+
+
+
+/* 2. Convert explicitly:
+
+"123" → number
+
+0 → boolean
+
+true → string
+
+num = "123"
+Number(num)
+console.log(num);
+
+Boolean(0) ---> dont know how to [print this one] ----> console.log(Boolean(0)) <----- this is how it be be printed
+
+x = true
+String(x)
+console.log(x); */
+
+
+/* 3. Predict:
+console.log([] == 0);
+console.log([1,2] == "1,2");
+console.log("" == false);
+
+// true
+// true 
+// true
+
+*/
--- datatype_excercise.js
+++ datatype_excercise.js
@@ -12,21 +12,32 @@ console.log(typeof null);
 // 0 // undefined
 // 0 // object:*/
 
-/* 2. Create variables for each primitive type (number, string, boolean, undefined, null, symbol, bigint) and console.log them.*/
+/* 2. Create variables for each primitive type (number, string, boolean, undefined, null, symbol, bigint) and console.log them.
 
 let number = 10
 console.log(typeof number)
 let name = "raju"
 console.log(typeof name);
 let IsLoggedIn = false;
-console.log(IsLoggedIn);
+console.log(typeof IsLoggedIn);
 let c;
-console.log(c);
+console.log(typeof c);
 let y = null;
-console.log(y);
+console.log(typeof y);
 let big = 1234567890123456789012345678901234567890n;
-console.log(big);
+console.log(typeof big); // --> this didnt worked
+*/
 
+
+/* 3. What will happen here? Why?
+
+let a = "10";
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+
+1) 105 ---> since a is string and b is integer here both of them are taken as string
+2) 5 ---> unlike 1st question here catenation of strings are not possible so both are taken as integers/numbers*/
 
 
 
