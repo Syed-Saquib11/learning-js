f80f3ff4535dc657484fe8a8e303780b95a66cf1
Syed Saquib Quadri <Syed1saquib2quadri@gmail.com>
2025-08-20 14:32:46 +0000

    datatypes practise

---
+++ datatype_excercise.js
@@ -0,0 +1,33 @@
+/* 1. Predict the output
+
+console.log(typeof 99);
+console.log(typeof "99");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+
+// Number // correct
+// String // correct
+// 1 // boolean, not 1
+// 0 // undefined
+// 0 // object:*/
+
+/* 2. Create variables for each primitive type (number, string, boolean, undefined, null, symbol, bigint) and console.log them.*/
+
+let number = 10
+console.log(typeof number)
+let name = "raju"
+console.log(typeof name);
+let IsLoggedIn = false;
+console.log(IsLoggedIn);
+let c;
+console.log(c);
+let y = null;
+console.log(y);
+let big = 1234567890123456789012345678901234567890n;
+console.log(big);
+
+
+
+
+
