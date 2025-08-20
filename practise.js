7bcfeacbb9ed70c88c216d62b1eaeea9a45b0a50
Syed Saquib Quadri <Syed1saquib2quadri@gmail.com>
2025-08-20 14:10:42 +0000

    datatypes basics

---
+++ practise.js
@@ -0,0 +1,20 @@
+// Practising basic DATATYPES
+
+let name = "Saquib"; 
+temp = `Hi ${name}, welcome`
+//console.log(temp) -----> used to give output
+
+
+// JAVASCRIPT IS DYNAMICALLY TYPED
+let x = 10; //---> this will now give output x as 10
+x = 'string'//---> now it changes to string, hence we did needed to change it explicitly, it got modified dynamically
+//console.log(x);
+
+
+// typeof is used to know the type of datatype
+console.log(typeof 42);        // "number"
+console.log(typeof "hello");   // "string"
+console.log(typeof true);      // "boolean"
+console.log(typeof null);      // "object"  (quirk in JS!)
+console.log(typeof undefined); // "undefined"
+
